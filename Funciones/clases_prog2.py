2b69a8f3dc563ae6885560a2e74752126382af02
Aulas de Informática FCE - 307 <aulasfce1@udistrital.edu.co>
2019-05-18 07:07:16 -0500

    Iniciando con clases

---
+++ Funciones/clases_prog2.py
@@ -0,0 +1,28 @@
+class Figura(object):
+	"""docstring for Figura"""
+	def __init__(self):
+		super(Figura, self).__init__()
+		#self.arg = arg
+        self.x = [-1,1,0,-1]
+        self.y = [0,0,1.5,0]
+		
+def rotar(self,x,y,angulo):
+    ''' Se retorna un arreglo x, y modificado rotando el valor de angulo (en rads) respecto a (0,0) '''
+    xr = [];   yr = []
+    for i in range(len(x)):
+        if x[i] == 0:
+            if y[i] >0:
+                ang = np.pi/2
+            else:
+                ang = -np.pi/2
+            r   = y[i]
+        else:
+            ang = np.math.atan(y[i]/x[i])
+            r   = x[i]/np.math.cos(ang)
+        xt  = r*np.math.cos(ang+angulo)
+        yt  = r*np.math.sin(ang+angulo)
+        
+        xr.append(xt);   yr.append(yt);
+    return xr,yr
+
+#print('Hi')
